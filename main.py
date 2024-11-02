79aea2896d3f797275f55f1317b150df00de4dd2
TASIE GODSTIME <onyemaechitasie@gmail.com>
2024-11-03 00:35:49 +0100

    Names Seperated

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ df = pandas.read_csv("data.csv", sep=",")
 col1, col2, col3 = st.columns(3)
 with col1:
     for index, row in df[:4].iterrows():
-        st.subheader(f"{row['first name'].title()}{row['last name'].title()}")
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row["role"])
         st.image("images/" + row["image"], width=300)
 
 with col2:
     for index, row in df[4:8].iterrows():
-        st.subheader(f"{row['first name'].title()}{row['last name'].title()}")
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row["role"])
         st.image("images/" + row["image"], width=300)
 
 with col3:
     for index, row in df[8:12].iterrows():
-        st.subheader(f"{row['first name'].title()}{row['last name'].title()}")
+        st.subheader(f"{row['first name'].title()} {row['last name'].title()}")
         st.write(row["role"])
         st.image("images/" + row["image"], width=300)
